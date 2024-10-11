aa114e32a2daa8c8519cd456741b0f1110d3e3e5
elena <elenathermo958@gmail.com>
2024-10-11 13:26:13 +0200

    Cambio colores y texto navegador

--- main.js
+++ main.js
@@ -13,8 +13,20 @@ console.log(companyData);
 const companyNameDOM = document.getElementById("companyName");
 companyNameDOM.textContent = companyData.name
 
+const companyName2DOM = document.getElementById("companyName2");
+companyName2DOM.textContent = companyData.name
+
 const companyImgDOM = document.getElementById("companyImage");
 companyImgDOM.setAttribute("src", companyData.imageUrl)
 
 const companyClaimDOM = document.getElementById("companyClaim")
 companyClaimDOM.textContent = companyData.claim
+
+const companyTextDOM = document.getElementById("companyText")
+companyTextDOM.textContent = companyData.text
+
+const companyPrimDOM = document.getElementById("primaryColorInput")
+companyPrimDOM.setAttribute("primaryColorInput", companyData.primaryColor)
+
+const companyBackDOM = document.getElementById("backgroundColorInput")
+companyBackDOM.setAttribute("backgroundColorInput", companyData.backgroundColor)
